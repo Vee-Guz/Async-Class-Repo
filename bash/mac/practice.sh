e70fe270a69a920e96dbca8b04dadd4707a15f75
Vee-Guz <vero.guzman48612@gmail.com>
2021-03-20 14:12:34 -0700

    Added new folders to orginize bash files for different systems

---
+++ bash/mac/practice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#for mac
+#create user
+#sudo dscl . create /User/BashUser
+
+#create group and add user to group
+#sudo dscl . create /Groups/practice RealName "Fake Users"
+#sudo dscl . append /Groups/practice GroupMemembership BashUser
+
+#copy file from other directory
+cp /Users/vero/DevopsExercises/myExercises/NonGithubFiles/hello.txt .
+
+#change permission on files, group rights -- can only read and execute file
+chmod 754 hello.txt
+
+#rm .txt file
+rm hello.txt
